bae9653dfd2de08b68caefcf55ebd67e16dabf17
Sayerimr <mireyasarda@gmail.com>
2025-03-31 22:05:45 +0200

    Codigo Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MenuComponent } from "./menu/menu.component";
 
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
